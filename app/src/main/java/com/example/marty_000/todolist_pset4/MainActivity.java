b825fb80ab64a74be25b75be150d7e99caa527fe
10800441 <martyn_heijstek@hotmail.com>
2016-11-25 13:16:09 +0100

    Pop up screen works

--- app/src/main/java/com/example/marty_000/todolist_pset4/MainActivity.java
+++ app/src/main/java/com/example/marty_000/todolist_pset4/MainActivity.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
-        import android.widget.AdapterView;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
         import android.widget.EditText;
         import android.widget.ListView;
@@ -23,25 +25,27 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences prefs;
     ArrayAdapter<ToDoItem> adapter;
     ArrayList<ToDoItem> toDoList;
-    private String editedText;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        context = getApplicationContext();
         editText = (EditText) findViewById(R.id.editText);
         listView = (ListView) findViewById(R.id.toDoListView);
         helper = new DBHelper(this);
 
+        // Load three toDoItems on first initialization
         prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         if (!prefs.contains("toDoList")) {
 
+
             helper.create(new ToDoItem("Welcome user!"));
-
             helper.create(new ToDoItem("Add your to-do items"));
-
-            helper.create(new ToDoItem("And remove them by longpress"));
+            helper.create(new ToDoItem("Tap to edit and remove them by longpress"));
 
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("toDoList", "toDoList");
@@ -49,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
         }
         updateAdapter();
     }
-        private void updateAdapter(){
+
+        // This function will update the listview
+    private void updateAdapter(){
         toDoList = helper.read();
 
-        // standard adapter
+        // Standard adapter
         adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, toDoList);
         listView.setAdapter(adapter);
@@ -61,59 +67,73 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
-                delete(pos);
+                helper.delete(adapter.getItem(pos));
+                updateAdapter();
                 return true;
             }
         });
 
+        // Tap an item to edit
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long id) {
-                Context context = getApplicationContext();
                 ToDoItem editableItem = adapter.getItem(pos);
 
                 if (editableItem != null) {
-                    final String updatedText = popUp(editableItem.text);
-                    helper.update(new ToDoItem(editableItem.id_number, updatedText));
-                    adapter.insert(new ToDoItem(editableItem.id_number, updatedText), pos);
+                    // Send the user an alert dialog
+                    popUp(editableItem.id_number, editableItem.text);
                     updateAdapter();
 
                 } else {
                     Toast.makeText(context, "item is not editable", Toast.LENGTH_SHORT).show();
                     updateAdapter();
                 }
-            }
+                }
         });
     }
 
-    public void delete(int pos) {
-        helper.delete(adapter.getItem(pos));
-        updateAdapter();
-    }
+
+    // Add a new item to the listView
     public void add(View view) {
         String todo_string = editText.getText().toString();
+        if(todo_string.length() !=0) {
+            ToDoItem item = new ToDoItem(todo_string);
+            helper.create(item);
+            editText.getText().clear();
+            updateAdapter();
 
-        ToDoItem item = new ToDoItem(todo_string);
-        helper.create(item);
-        editText.getText().clear();
-        updateAdapter();
+        } else {
+            Toast.makeText(context, "Please enter text!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public String popUp(String popUpText){
+    // Pop up screen that gives the user the ability to change an item
+    public void popUp(final int id, String popUpText){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
         alert.setTitle("Update ToDo");
         alert.setMessage("Change the text of your to do item");
 
+        // Open key board
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+
         // Set an EditText view to get user input
         final EditText input = new EditText(this);
         input.setText(popUpText);
         alert.setView(input);
+        // Set the cursor to the right of the text
+        input.setSelection(input.getText().length());
 
+        // Change the ToDoItem
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
-                MainActivity.this.editedText = input.getText().toString();
+                if( input.getText().length() != 0){
+                    helper.update(new ToDoItem(id, input.getText().toString()));
+                } else {
+                    Toast.makeText(context, "Please enter text!", Toast.LENGTH_SHORT).show();
+                }
+                updateAdapter();
             }
         });
 
@@ -123,6 +143,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         alert.show();
-        return editedText;
     }
+
 }
